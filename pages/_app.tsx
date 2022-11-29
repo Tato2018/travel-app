60802982bc6784130219146a49dc7a166a4f80c0
Tato2018 <tatoabramishvili2018@gmail.com>
2022-11-29 15:51:11 +0400

    move font styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
+// import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
